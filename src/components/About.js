cd9f21126fd3e6407131412993bdaa5642516f33
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-12-03 16:48:17 -0600

    classe is my favorite thing to pair with coffee

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Links from "./Links";
 
-function About(props) {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links github={props.links.github} linkedin={props.links.linkedin} />
-    </div>
-  );
+class About extends React.Component {
+  render() {
+    const { github, linkedin } = this.props.links;
+
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        {this.classMethod()}
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={github} linkedin={linkedin} />
+      </div>
+    );
+  }
+
+  classMethod() {
+    const { bio } = this.props;
+    return bio && bio.length > 1 ? <p>{bio}</p> : null;
+  }
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Home(props) {
-  return (
-    <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.username} is a Web Developer from {props.city}
-      </h1>
-    </div>
-  );
+class Home extends React.Component {
+  render() {
+    return (
+      <div id="home">
+        <h1 style={{ color: this.props.color }}>
+          {this.classMethod()}
+        </h1>
+      </div>
+    );
+  }
+
+  classMethod(){
+    const {username , city} = this.props
+    return (
+      `${username} is a Web Developer from ${city}`
+     )
+  }
+
 }
 
 export default Home;
